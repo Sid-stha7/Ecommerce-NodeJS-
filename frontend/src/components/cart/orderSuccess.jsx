38cab2a53ac769f6911bd64e6b0ba9aae0bc8689
sid_stha7 <sidstha444@gmail.com>
2022-11-08 10:28:06 +0545

    node mailer

--- frontend/src/components/cart/orderSuccess.jsx
+++ frontend/src/components/cart/orderSuccess.jsx
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import MetaData from '../MetaData';
+
+import { useDispatch } from 'react-redux';
 
 const OrderSuccess = () => {
   return (
@@ -11,13 +14,15 @@ const OrderSuccess = () => {
         <div className="col-6 mt-5 text-center">
           <img
             className="my-5 img-fluid d-block mx-auto"
-            src="/images/order_success.png"
+            src="/images/success.png"
             alt="Order Success"
             width="200"
             height="200"
           />
 
           <h2>Your Order has been placed successfully.</h2>
+
+          <h2 className="text-success">Order Email has been sent</h2>
 
           <Link to="/orders/me">Go to Orders</Link>
         </div>
